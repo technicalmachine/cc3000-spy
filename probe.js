52fe7e605e90429c9c76b476c14e5c10e34a1450
Tim Cameron Ryan <id@timryan.org>
2014-07-19 11:53:56 -0700

    Minor fw fix

--- probe.js
+++ probe.js
@@ -30,7 +30,7 @@ exports.probe = function (next) {
       console.log('You can install this (for example on OS X) with this following (in a temp dir):');
       console.log('  brew install rpm2cpio');
       console.log('  wget ftp://ftp.pbone.net/mirror/ftp5.gwdg.de/pub/opensuse/repositories/home:/Heinervdm:/sigrok/openSUSE_Tumbleweed/noarch/sigrok-firmware-fx2lafw-0.1.1-4.1.noarch.rpm');
-      console.log('  rpm2cpio.pl sigrok-firmware-fx2lafw-0.1.1-4.1.noarch.rpm')
+      console.log('  rpm2cpio.pl sigrok-firmware-fx2lafw-0.1.1-4.1.noarch.rpm | cpio -idmv')
       console.log('  cp ./usr/share/sigrok-firmware/fx2lafw-saleae-logic.fw .')
       console.log('  rm -rf ./usr');
       console.log('  mv fx2lafw-saleae-logic.fw ' + String(data).match(/\S+fx2lafw\-saleae\-logic\.fw/)[0]);
