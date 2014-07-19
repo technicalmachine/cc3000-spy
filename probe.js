a67f9a8ff7a1bbe5dfa155ed9d1aa01cfa168e3f
Tim Cameron Ryan <id@timryan.org>
2014-07-19 11:47:46 -0700

    Adds instructions for installing saleae drivers.

--- probe.js
+++ probe.js
@@ -24,6 +24,19 @@ exports.probe = function (next) {
     '--probes', '0=cs,1=miso,2=irq,3=mosi,4=sck,5=sw_en'
   ])
   sig.stderr.on('data', function (data) {
+    if (String(data).indexOf('fx2lafw-saleae-logic.fw') > -1) {
+      console.log('ERR'.red, 'Missing the Saleae logic driver.');
+      console.log('');
+      console.log('You can install this (for example on OS X) with this following (in a temp dir):');
+      console.log('  brew install rpm2cpio');
+      console.log('  wget ftp://ftp.pbone.net/mirror/ftp5.gwdg.de/pub/opensuse/repositories/home:/Heinervdm:/sigrok/openSUSE_Tumbleweed/noarch/sigrok-firmware-fx2lafw-0.1.1-4.1.noarch.rpm');
+      console.log('  rpm2cpio.pl sigrok-firmware-fx2lafw-0.1.1-4.1.noarch.rpm')
+      console.log('  cp ./usr/share/sigrok-firmware/fx2lafw-saleae-logic.fw .')
+      console.log('  rm -rf ./usr');
+      console.log('  mv fx2lafw-saleae-logic.fw ' + String(data).match(/\S+fx2lafw\-saleae\-logic\.fw/)[0]);
+      console.log('Then re-running.');
+      console.log('');
+    }
     if (String(data).match(/Device stopped/)) {
       sig.kill();
       console.error(String(data).grey);
